a9188e251a3d4a1b0b9bd6622ab67a7c22f6a17e
irfanghapar <irfanghapar02@gmail.com>
2024-01-02 00:00:00 +0800

    Update dependencies and adjust layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,6 +1,5 @@
 // New line added 2024-09-22T19:21:39.135Z
   @moduledoc """
-// New line added 2024-09-22T19:21:32.012Z
 // New line added 2024-09-22T19:21:36.241Z
   See the `layouts` directory for all templates available.
   layout on both `use CerberusWeb, :controller` and
