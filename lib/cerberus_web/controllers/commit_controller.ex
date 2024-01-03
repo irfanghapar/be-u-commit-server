ddee214f168b6f071100a795c3169522f5b4c50e
irfanghapar <irfanghapar02@gmail.com>
2024-01-04 00:00:00 +0800

    Implement new API endpoint in commit_controller.ex

--- lib/cerberus_web/controllers/commit_controller.ex
+++ lib/cerberus_web/controllers/commit_controller.ex
@@ -9,6 +9,7 @@ defmodule CerberusWeb.CommitController do
 // New line added 2024-09-22T19:21:28.784Z
   def developer_count_by_year(conn, %{"year" => year}) do
       {year_int, _} when year_int > 0 ->
+// New line added 2024-09-22T19:32:41.927Z
         commit_counts = Commit.get_developer_count_by_date(year)
         json(conn, %{data: commit_counts})
       _ ->
