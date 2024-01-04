31e2ac1db2c928da00e01d015ab3e80f9be6f544
irfanghapar <irfanghapar02@gmail.com>
2024-01-05 00:00:00 +0800

    Implement rate limiting in tests/authTests.js

--- tests/authTests.js
+++ tests/authTests.js
@@ -37,3 +37,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:55.687Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:57.726Z
+// TODO: Implement functionality
