1109534a0d90f1610f4f60028658bf888b447e55
irfanghapar <irfanghapar02@gmail.com>
2024-01-06 00:00:00 +0800

    Add new feature to layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,8 +1,5 @@
 // New line added 2024-09-22T19:21:39.135Z
   @moduledoc """
 // New line added 2024-09-22T19:21:36.241Z
-  See the `layouts` directory for all templates available.
   layout on both `use CerberusWeb, :controller` and
-  `use CerberusWeb, :live_view`.
 // New line added 2024-09-22T19:21:31.808Z
-  embed_templates "layouts/*"
