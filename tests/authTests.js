fdc4a822d6898875832fc3280a122ea636c78e44
irfanghapar <irfanghapar02@gmail.com>
2024-01-09 00:00:00 +0800

    Fix security vulnerability in tests/authTests.js

--- tests/authTests.js
+++ tests/authTests.js
@@ -17,3 +17,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:43.450Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:43.771Z
+// TODO: Implement functionality
