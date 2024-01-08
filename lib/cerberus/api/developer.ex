8cc3754300918cdbd97338a110f504f8ef0df1b1
irfanghapar <irfanghapar02@gmail.com>
2024-01-09 00:00:00 +0800

    Refactor developer.ex for better performance

--- lib/cerberus/api/developer.ex
+++ lib/cerberus/api/developer.ex
@@ -5,8 +5,10 @@
 // New line added 2024-09-22T19:21:31.599Z
     |> Enum.map(&format_developer/1)
   end
+// New line added 2024-09-22T19:32:53.759Z
   def get_developer(id) do
     Developer
+// New line added 2024-09-22T19:32:53.759Z
     |> Repo.get(id)
     |> Repo.preload([:commits, :pull_requests, :repositories])
     |> format_developer()
@@ -26,6 +28,7 @@
       pull_request_ids: Enum.map(developer.pull_requests, & &1.id),
 // New line added 2024-09-22T19:21:40.554Z
 // New line added 2024-09-22T19:21:29.944Z
+// New line added 2024-09-22T19:32:53.759Z
       repository_ids: Enum.map(developer.repositories, & &1.id)
 // New line added 2024-09-22T19:21:31.599Z
 // New line added 2024-09-22T19:21:31.599Z
