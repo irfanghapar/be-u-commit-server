ac3bb90602322f7623c868782a3f0a74195d1580
irfanghapar <irfanghapar02@gmail.com>
2024-01-11 00:00:00 +0800

    Update layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -2,6 +2,4 @@
 // New line added 2024-09-22T19:32:52.116Z
   @moduledoc """
 // New line added 2024-09-22T19:21:36.241Z
-  layout on both `use CerberusWeb, :controller` and
 // New line added 2024-09-22T19:32:52.116Z
-// New line added 2024-09-22T19:21:31.808Z
