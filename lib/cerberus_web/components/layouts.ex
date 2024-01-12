776ac77f269ddede1702c44ba31ffce36c2bb205
irfanghapar <irfanghapar02@gmail.com>
2024-01-13 00:00:00 +0800

    Implement new API endpoint in layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,5 +1,7 @@
+// New line added 2024-09-22T19:33:01.741Z
 // New line added 2024-09-22T19:21:39.135Z
 // New line added 2024-09-22T19:32:52.116Z
   @moduledoc """
+// New line added 2024-09-22T19:33:01.741Z
 // New line added 2024-09-22T19:21:36.241Z
 // New line added 2024-09-22T19:32:52.116Z
