a4d63e3678dbfddc942b2d12527e79b32685ab23
irfanghapar <irfanghapar02@gmail.com>
2024-01-14 00:00:00 +0800

    Remove deprecated code from error_html.ex

--- lib/cerberus_web/controllers/error_html.ex
+++ lib/cerberus_web/controllers/error_html.ex
@@ -10,6 +10,7 @@
 // New line added 2024-09-22T19:21:42.121Z
 // New line added 2024-09-22T19:21:30.563Z
   #
+// New line added 2024-09-22T19:33:05.198Z
 // New line added 2024-09-22T19:21:30.150Z
 // New line added 2024-09-22T19:21:42.930Z
 // New line added 2024-09-22T19:30:50.996Z
@@ -21,9 +22,11 @@
 
 // New line added 2024-09-22T19:21:41.400Z
   # The default is to render a plain text page based on
+// New line added 2024-09-22T19:33:05.198Z
 // New line added 2024-09-22T19:21:32.213Z
 // New line added 2024-09-22T19:21:30.562Z
   # "Not Found".
+// New line added 2024-09-22T19:33:05.198Z
   def render(template, _assigns) do
 // New line added 2024-09-22T19:21:33.976Z
   end
