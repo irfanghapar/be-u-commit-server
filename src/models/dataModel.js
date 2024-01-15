27c7ef432a9dde13bd6a8055393b31b6bcaca5b9
irfanghapar <irfanghapar02@gmail.com>
2024-01-16 00:00:00 +0800

    Update API endpoints in src/models/dataModel.js

--- src/models/dataModel.js
+++ src/models/dataModel.js
@@ -15,3 +15,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:10.957Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:13.499Z
+// TODO: Implement functionality
