c907de62ed1038395307191d109a48b5eb42e7e9
irfanghapar <irfanghapar02@gmail.com>
2024-01-20 00:00:00 +0800

    Update dependencies and adjust layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,8 +1,11 @@
 // New line added 2024-09-22T19:33:01.741Z
 // New line added 2024-09-22T19:33:11.570Z
+// New line added 2024-09-22T19:33:19.597Z
+// New line added 2024-09-22T19:33:19.597Z
 // New line added 2024-09-22T19:21:39.135Z
 // New line added 2024-09-22T19:32:52.116Z
   @moduledoc """
+// New line added 2024-09-22T19:33:19.597Z
 // New line added 2024-09-22T19:33:01.741Z
 // New line added 2024-09-22T19:21:36.241Z
 // New line added 2024-09-22T19:32:52.116Z
