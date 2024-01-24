70d7681c831396ed925d74a7d70f8c4b728cd678
irfanghapar <irfanghapar02@gmail.com>
2024-01-25 00:00:00 +0800

    Improve error handling in src/middleware/errorMiddleware.js

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -27,3 +27,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:38.743Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:38.852Z
+// TODO: Implement functionality
