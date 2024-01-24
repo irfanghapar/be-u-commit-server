1a150ac3c064e714b9d1b4b1967954f40e6d6432
irfanghapar <irfanghapar02@gmail.com>
2024-01-25 00:00:00 +0800

    Add unit tests for src/middleware/errorMiddleware.js

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -25,3 +25,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:32.559Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:38.743Z
+// TODO: Implement functionality
