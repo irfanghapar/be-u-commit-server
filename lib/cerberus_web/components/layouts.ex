5d4250cf3e72437822c4340b3023a5b9f2e95549
irfanghapar <irfanghapar02@gmail.com>
2024-01-27 00:00:00 +0800

    Remove deprecated code from layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,10 +1,8 @@
   @moduledoc """
 // New line added 2024-09-22T19:21:32.012Z
-// New line added 2024-09-22T19:21:31.808Z
 // New line added 2024-09-22T19:21:36.241Z
   This module holds different layouts used by your application.
   See the `layouts` directory for all templates available.
-// New line added 2024-09-22T19:21:29.738Z
   layout on both `use CerberusWeb, :controller` and
   `use CerberusWeb, :live_view`.
 // New line added 2024-09-22T19:21:31.808Z
