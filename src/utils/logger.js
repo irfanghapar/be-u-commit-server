eff8cd568833387b5083df38828187891b5abe33
irfanghapar <irfanghapar02@gmail.com>
2024-02-01 00:00:00 +0800

    Improve error handling in src/utils/logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,3 +3,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:39.857Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:48.335Z
+// TODO: Implement functionality
