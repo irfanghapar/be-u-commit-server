767478c1f4553db1a126b5d531146e6d86826992
irfanghapar <irfanghapar02@gmail.com>
2024-02-01 00:00:00 +0800

    Add caching layer to src/models/userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,3 +3,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:43.557Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:48.121Z
+// TODO: Implement functionality
