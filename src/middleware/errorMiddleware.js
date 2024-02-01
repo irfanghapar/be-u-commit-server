1b20ed2b4183aeb31a9001ac6d42f7f4e3ca9108
irfanghapar <irfanghapar02@gmail.com>
2024-02-02 00:00:00 +0800

    Implement rate limiting in src/middleware/errorMiddleware.js

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -7,3 +7,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:45.501Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:55.794Z
+// TODO: Implement functionality
