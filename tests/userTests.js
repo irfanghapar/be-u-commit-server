18585ea39767c7cfa57e39186dc7c39a798e86c1
irfanghapar <irfanghapar02@gmail.com>
2024-02-02 00:00:00 +0800

    Add logging system

--- tests/userTests.js
+++ tests/userTests.js
@@ -41,3 +41,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:55.472Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:55.578Z
+// TODO: Implement functionality
