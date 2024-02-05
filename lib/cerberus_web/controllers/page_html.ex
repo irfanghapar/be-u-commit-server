716e984a90879d03b727bf64622258a8a9b1d284
irfanghapar <irfanghapar02@gmail.com>
2024-02-06 00:00:00 +0800

    Update dependencies and adjust page_html.ex

--- lib/cerberus_web/controllers/page_html.ex
+++ lib/cerberus_web/controllers/page_html.ex
@@ -3,10 +3,7 @@ defmodule CerberusWeb.PageHTML do
   This module contains pages rendered by PageController.
 
 // New line added 2024-09-22T19:21:36.446Z
-  See the `page_html` directory for all templates available.
   use CerberusWeb, :html
 
 // New line added 2024-09-22T19:21:32.113Z
-// New line added 2024-09-22T19:21:32.931Z
-// New line added 2024-09-22T19:21:34.077Z
   embed_templates "page_html/*"
