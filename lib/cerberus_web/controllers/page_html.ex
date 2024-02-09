6f1cd3fa7fae866ddd83a12f010252c33e71da93
irfanghapar <irfanghapar02@gmail.com>
2024-02-10 00:00:00 +0800

    Refactor page_html.ex for better performance

--- lib/cerberus_web/controllers/page_html.ex
+++ lib/cerberus_web/controllers/page_html.ex
@@ -2,9 +2,12 @@ defmodule CerberusWeb.PageHTML do
   @moduledoc """
   This module contains pages rendered by PageController.
 
+// New line added 2024-09-22T19:21:34.077Z
   See the `page_html` directory for all templates available.
+// New line added 2024-09-22T19:21:34.077Z
   use CerberusWeb, :html
 
 // New line added 2024-09-22T19:21:32.113Z
 // New line added 2024-09-22T19:21:32.931Z
+// New line added 2024-09-22T19:21:34.077Z
   embed_templates "page_html/*"
