3c7508264dded0870ea7e2f17fcfef5890ac8d34
irfanghapar <irfanghapar02@gmail.com>
2024-02-11 00:00:00 +0800

    Optimize database queries in src/middleware/authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -17,3 +17,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:15.787Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:16.002Z
+// TODO: Implement functionality
