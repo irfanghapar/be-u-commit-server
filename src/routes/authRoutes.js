db4adbd7034d76034f1be682c9d3a4cff43683f2
irfanghapar <irfanghapar02@gmail.com>
2024-02-16 00:00:00 +0800

    Update dependencies and adjust src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,3 +19,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:19.784Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:24.440Z
+// TODO: Implement functionality
