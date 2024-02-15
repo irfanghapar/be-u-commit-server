5af2a8017b6b57ef3976b390ca4698ff8c5d7ddd
irfanghapar <irfanghapar02@gmail.com>
2024-02-16 00:00:00 +0800

    Optimize database queries in src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -23,3 +23,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:25.412Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:25.956Z
+// TODO: Implement functionality
