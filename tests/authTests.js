84ad8bedc24eb394d6513d53e14f22fcf9c884fe
irfanghapar <irfanghapar02@gmail.com>
2024-02-17 00:00:00 +0800

    Improve error handling in tests/authTests.js

--- tests/authTests.js
+++ tests/authTests.js
@@ -45,3 +45,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:02.680Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:04.202Z
+// TODO: Implement functionality
