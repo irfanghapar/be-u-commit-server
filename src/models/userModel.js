91f71d0643b0557edf891988360f8eb6092a0404
irfanghapar <irfanghapar02@gmail.com>
2024-02-24 00:00:00 +0800

    Update API endpoints in src/models/userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -21,3 +21,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:59.797Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:01.814Z
+// TODO: Implement functionality
