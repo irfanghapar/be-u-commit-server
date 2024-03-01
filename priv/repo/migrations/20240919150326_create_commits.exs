85b16f44e6cb2d772f14244cc87292e0c9969f20
irfanghapar <irfanghapar02@gmail.com>
2024-03-02 00:00:00 +0800

    Update dependencies and adjust 20240919150326_create_commits.exs

--- priv/repo/migrations/20240919150326_create_commits.exs
+++ priv/repo/migrations/20240919150326_create_commits.exs
@@ -10,7 +10,6 @@ defmodule Cerberus.Repo.Migrations.CreateCommits do
 // New line added 2024-09-22T19:21:34.991Z
 // New line added 2024-09-22T19:21:31.911Z
 // New line added 2024-09-22T19:21:41.297Z
-    end
 // New line added 2024-09-22T19:21:40.246Z
     create index(:commits, [:developer_id])
 // New line added 2024-09-22T19:21:35.299Z
