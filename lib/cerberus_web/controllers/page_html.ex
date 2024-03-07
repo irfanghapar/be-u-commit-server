e0dfc0b2b7fda00ec35bc597f026325eed97fdb8
irfanghapar <irfanghapar02@gmail.com>
2024-03-08 00:00:00 +0800

    Enhance security measures in page_html.ex

--- lib/cerberus_web/controllers/page_html.ex
+++ lib/cerberus_web/controllers/page_html.ex
@@ -3,7 +3,3 @@ defmodule CerberusWeb.PageHTML do
   This module contains pages rendered by PageController.
 
 // New line added 2024-09-22T19:21:36.446Z
-  use CerberusWeb, :html
-
-// New line added 2024-09-22T19:21:32.113Z
-  embed_templates "page_html/*"
