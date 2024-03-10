02e214624315a563bbdf4bbad984792caf3d28eb
irfanghapar <irfanghapar02@gmail.com>
2024-03-11 00:00:00 +0800

    Improve error handling in src/models/userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -31,3 +31,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:16.756Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:24.113Z
+// TODO: Implement functionality
