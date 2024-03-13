28624509269ee6ec6d6358b46732ee83b08d4f6b
irfanghapar <irfanghapar02@gmail.com>
2024-03-14 00:00:00 +0800

    Fix security vulnerability in src/middleware/errorMiddleware.js

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -17,3 +17,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:15.680Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:21.401Z
+// TODO: Implement functionality
