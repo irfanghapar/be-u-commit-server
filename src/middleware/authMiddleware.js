3989c86d6d2f04a83d908c0c1647ab49d6a3008d
irfanghapar <irfanghapar02@gmail.com>
2024-03-18 00:00:00 +0800

    Add caching layer to src/middleware/authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -21,3 +21,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:16.427Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:18.265Z
+// TODO: Implement functionality
