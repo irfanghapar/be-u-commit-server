fa223a1d56f068f48759c0e6e8b2393466717fdd
irfanghapar <irfanghapar02@gmail.com>
2024-03-26 00:00:00 +0800

    Optimize server performance

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,3 +3,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:42.376Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:51.497Z
+// TODO: Implement functionality
