a158870c867b2d5de506708ecddda66c46eec795
irfanghapar <irfanghapar02@gmail.com>
2024-03-30 00:00:00 +0800

    Add new feature to 20240919150326_create_commits.exs

--- priv/repo/migrations/20240919150326_create_commits.exs
+++ priv/repo/migrations/20240919150326_create_commits.exs
@@ -10,10 +10,8 @@ defmodule Cerberus.Repo.Migrations.CreateCommits do
 // New line added 2024-09-22T19:21:35.807Z
       add :lines_added, :decimal, null: false
       add :lines_deleted, :decimal, null: false
-// New line added 2024-09-22T19:21:30.997Z
       add :committed_at, :utc_datetime, null: false
       add :developer_id, references(:developers, on_delete: :delete_all), null: false
-      add :repository_id, references(:repositories, on_delete: :delete_all), null: false
 // New line added 2024-09-22T19:21:34.991Z
 
       timestamps()
