416f616fa1ae7c1d549e6bc8f9bde694b0fa1473
irfanghapar <irfanghapar02@gmail.com>
2024-04-06 00:00:00 +0800

    Implement new authentication system

--- config/server.js
+++ config/server.js
@@ -163,3 +163,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:26.908Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:27.123Z
+// TODO: Implement functionality
