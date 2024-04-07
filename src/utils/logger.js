886b469365cac73e0df399be5e40a59e67b7b290
irfanghapar <irfanghapar02@gmail.com>
2024-04-08 00:00:00 +0800

    Fix security vulnerability in src/utils/logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -25,3 +25,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:34.393Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:36.119Z
+// TODO: Implement functionality
