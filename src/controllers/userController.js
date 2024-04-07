86c73d5ba2582b930c533aae404947aac91386ab
irfanghapar <irfanghapar02@gmail.com>
2024-04-08 00:00:00 +0800

    Fix security vulnerability in src/controllers/userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,3 +19,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:35.582Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:36.334Z
+// TODO: Implement functionality
