41b078f7b5bcf2692ffbb103c0bcd172b0153939
irfanghapar <irfanghapar02@gmail.com>
2024-04-20 00:00:00 +0800

    Optimize database queries in tests/userTests.js

--- tests/userTests.js
+++ tests/userTests.js
@@ -29,3 +29,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:45.720Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:46.259Z
+// TODO: Implement functionality
