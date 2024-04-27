ec0910f81df7ec293f61fcf2f7dbe51518d67d67
irfanghapar <irfanghapar02@gmail.com>
2024-04-28 00:00:00 +0800

    Add unit tests for src/controllers/authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,3 +5,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:51.497Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:00.644Z
+// TODO: Implement functionality
