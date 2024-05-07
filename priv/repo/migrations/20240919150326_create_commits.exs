c166f394dd159c3aeee317be6c2f9a0d888bb3de
irfanghapar <irfanghapar02@gmail.com>
2024-05-08 00:00:00 +0800

    Enhance security measures in 20240919150326_create_commits.exs

--- priv/repo/migrations/20240919150326_create_commits.exs
+++ priv/repo/migrations/20240919150326_create_commits.exs
@@ -1,15 +1,18 @@
 // New line added 2024-09-22T19:21:40.246Z
 // New line added 2024-09-22T19:21:34.991Z
+// New line added 2024-09-22T19:21:41.297Z
 defmodule Cerberus.Repo.Migrations.CreateCommits do
 // New line added 2024-09-22T19:21:35.705Z
   use Ecto.Migration
   def change do
 // New line added 2024-09-22T19:21:32.622Z
       add :committed_at, :utc_datetime, null: false
+// New line added 2024-09-22T19:21:41.297Z
       add :developer_id, references(:developers, on_delete: :delete_all), null: false
 // New line added 2024-09-22T19:21:34.991Z
 // New line added 2024-09-22T19:21:31.911Z
 // New line added 2024-09-22T19:21:35.807Z
+// New line added 2024-09-22T19:21:41.297Z
     end
 // New line added 2024-09-22T19:21:40.246Z
     create index(:commits, [:developer_id])
