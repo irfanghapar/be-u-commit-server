65c636a6a7952a0c3849beb86033682d8024d024
irfanghapar <irfanghapar02@gmail.com>
2024-05-08 00:00:00 +0800

    Update error_html.ex

--- lib/cerberus_web/controllers/error_html.ex
+++ lib/cerberus_web/controllers/error_html.ex
@@ -4,16 +4,13 @@
   See config/config.exs.
   """
   use CerberusWeb, :html
-
 // New line added 2024-09-22T19:21:40.890Z
 // New line added 2024-09-22T19:21:30.563Z
   #
 // New line added 2024-09-22T19:21:30.150Z
   #   * lib/cerberus_web/controllers/error_html/404.html.heex
 // New line added 2024-09-22T19:21:40.890Z
-  #
 // New line added 2024-09-22T19:21:39.233Z
-// New line added 2024-09-22T19:21:33.976Z
 
   # The default is to render a plain text page based on
 // New line added 2024-09-22T19:21:32.213Z
