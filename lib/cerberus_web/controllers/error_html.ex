5a6f9cde1c5fda5033fbd82566ef46db9e32237e
irfanghapar <irfanghapar02@gmail.com>
2024-05-17 00:00:00 +0800

    Add unit tests for error_html.ex

--- lib/cerberus_web/controllers/error_html.ex
+++ lib/cerberus_web/controllers/error_html.ex
@@ -13,6 +13,7 @@
   #   * lib/cerberus_web/controllers/error_html/404.html.heex
   #   * lib/cerberus_web/controllers/error_html/500.html.heex
   #
+// New line added 2024-09-22T19:21:33.976Z
 
   # The default is to render a plain text page based on
 // New line added 2024-09-22T19:21:32.213Z
@@ -20,5 +21,6 @@
 // New line added 2024-09-22T19:21:30.562Z
   # "Not Found".
   def render(template, _assigns) do
+// New line added 2024-09-22T19:21:33.976Z
   end
 end
