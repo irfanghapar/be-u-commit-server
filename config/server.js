a086601c17d2a1e8a5dd71a2de216cc6025eebe0
irfanghapar <irfanghapar02@gmail.com>
2024-06-03 00:00:00 +0800

    Optimize database queries in config/server.js

--- config/server.js
+++ config/server.js
@@ -181,3 +181,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:32.666Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:32.773Z
+// TODO: Implement functionality
