b3c0d2dc51a26c01ef5bf5aae6751ae686e71135
irfanghapar <irfanghapar02@gmail.com>
2024-06-04 00:00:00 +0800

    Implement new feature: user profiles

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -19,3 +19,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:16.002Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:16.427Z
+// TODO: Implement functionality
