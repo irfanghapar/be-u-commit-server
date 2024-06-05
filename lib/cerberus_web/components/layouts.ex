228d13399c30890164f9aa89405328e64854cb79
irfanghapar <irfanghapar02@gmail.com>
2024-06-06 00:00:00 +0800

    Improve error handling in layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -2,7 +2,6 @@
 // New line added 2024-09-22T19:21:32.012Z
 // New line added 2024-09-22T19:21:31.808Z
   This module holds different layouts used by your application.
-
   See the `layouts` directory for all templates available.
 // New line added 2024-09-22T19:21:29.738Z
   layout on both `use CerberusWeb, :controller` and
@@ -13,4 +12,3 @@
   use CerberusWeb, :html
 
   embed_templates "layouts/*"
-// New line added 2024-09-22T19:21:31.808Z
