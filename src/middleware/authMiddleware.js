ca116b1f3ba22c1c846b5437b147ad39934a05eb
irfanghapar <irfanghapar02@gmail.com>
2024-06-16 00:00:00 +0800

    Optimize server performance

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,3 +5,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:59.471Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:01.307Z
+// TODO: Implement functionality
