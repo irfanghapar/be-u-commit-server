c338b77ad35c13ed98ea631628dcc6858053f200
irfanghapar <irfanghapar02@gmail.com>
2024-06-20 00:00:00 +0800

    Refactor 20240919150326_create_commits.exs for better performance

--- priv/repo/migrations/20240919150326_create_commits.exs
+++ priv/repo/migrations/20240919150326_create_commits.exs
@@ -1,3 +1,4 @@
+// New line added 2024-09-22T19:21:40.246Z
 // New line added 2024-09-22T19:21:34.991Z
 defmodule Cerberus.Repo.Migrations.CreateCommits do
 // New line added 2024-09-22T19:21:35.705Z
@@ -11,6 +12,7 @@ defmodule Cerberus.Repo.Migrations.CreateCommits do
 // New line added 2024-09-22T19:21:31.911Z
 // New line added 2024-09-22T19:21:35.807Z
     end
+// New line added 2024-09-22T19:21:40.246Z
     create index(:commits, [:developer_id])
 // New line added 2024-09-22T19:21:35.299Z
 end
