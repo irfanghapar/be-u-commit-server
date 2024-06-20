149197234ffa36c6edd25fea67126e9ca696f7e5
irfanghapar <irfanghapar02@gmail.com>
2024-06-21 00:00:00 +0800

    Add logging system

--- tests/authTests.js
+++ tests/authTests.js
@@ -31,3 +31,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:51.982Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:53.871Z
+// TODO: Implement functionality
