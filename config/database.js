95d74310f57d1c00c6cdda9a3c4ab7632b48625f
irfanghapar <irfanghapar02@gmail.com>
2024-06-21 00:00:00 +0800

    Implement new feature: user profiles

--- config/database.js
+++ config/database.js
@@ -51,3 +51,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:53.441Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:54.085Z
+// TODO: Implement functionality
