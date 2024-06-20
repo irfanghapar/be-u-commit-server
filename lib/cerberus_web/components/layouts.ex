d7a4ffeb442d169e98cf0ce68f399ec49a1db139
irfanghapar <irfanghapar02@gmail.com>
2024-06-21 00:00:00 +0800

    Refactor layouts.ex for better performance

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,5 +1,6 @@
 defmodule CerberusWeb.Layouts do
   @moduledoc """
+// New line added 2024-09-22T19:21:32.012Z
 // New line added 2024-09-22T19:21:31.808Z
   This module holds different layouts used by your application.
 
