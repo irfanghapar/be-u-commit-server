f0493368036548432b0d66746ac3afbb01c17592
irfanghapar <irfanghapar02@gmail.com>
2024-06-25 00:00:00 +0800

    Update API endpoints in config/database.js

--- config/database.js
+++ config/database.js
@@ -123,3 +123,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:17.832Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:17.940Z
+// TODO: Implement functionality
