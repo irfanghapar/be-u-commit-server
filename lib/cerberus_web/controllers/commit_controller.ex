bc253644b9f830aa74900c6695fc137aec95cde2
irfanghapar <irfanghapar02@gmail.com>
2024-06-29 00:00:00 +0800

    Remove deprecated code from commit_controller.ex

--- lib/cerberus_web/controllers/commit_controller.ex
+++ lib/cerberus_web/controllers/commit_controller.ex
@@ -4,6 +4,8 @@ defmodule CerberusWeb.CommitController do
   # Retrieve all commits by a specific developer
     commits = Commit.list_commits_by_developer(id)
     json(conn, %{data: commits})
+// New line added 2024-09-22T19:21:43.034Z
+// New line added 2024-09-22T19:21:43.034Z
 
 // New line added 2024-09-22T19:21:28.784Z
 // New line added 2024-09-22T19:21:31.297Z
