70026367ab0c1caadf9102b313827147127522a0
irfanghapar <irfanghapar02@gmail.com>
2024-07-10 00:00:00 +0800

    Add logging system

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,3 +11,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:05.692Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:06.576Z
+// TODO: Implement functionality
