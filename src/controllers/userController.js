c13f5a8c2adb3334ad55ff1af466d8d97bedd372
irfanghapar <irfanghapar02@gmail.com>
2024-07-10 00:00:00 +0800

    Optimize server performance

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,3 +9,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:05.099Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:06.686Z
+// TODO: Implement functionality
