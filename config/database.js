12b385a28934602d306f2446b6a78aba3777e7bc
irfanghapar <irfanghapar02@gmail.com>
2024-07-21 00:00:00 +0800

    Improve error handling in config/database.js

--- config/database.js
+++ config/database.js
@@ -85,3 +85,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:03.117Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:03.333Z
+// TODO: Implement functionality
