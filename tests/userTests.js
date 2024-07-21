6548687d0f4cb66deb200855867f4b06e09540ef
irfanghapar <irfanghapar02@gmail.com>
2024-07-22 00:00:00 +0800

    Improve error handling in tests/userTests.js

--- tests/userTests.js
+++ tests/userTests.js
@@ -107,3 +107,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:23.897Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:25.632Z
+// TODO: Implement functionality
