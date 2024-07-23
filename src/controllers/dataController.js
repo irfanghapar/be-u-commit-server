ef34dae35b7185f1f8b03e2b1dffcd45208581e4
irfanghapar <irfanghapar02@gmail.com>
2024-07-24 00:00:00 +0800

    Update API endpoints in src/controllers/dataController.js

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -3,3 +3,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:39.325Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:44.742Z
+// TODO: Implement functionality
