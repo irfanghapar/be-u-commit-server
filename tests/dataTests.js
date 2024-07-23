8a9ca16b0f9272a99834702e3c23ed76788816eb
irfanghapar <irfanghapar02@gmail.com>
2024-07-24 00:00:00 +0800

    Implement new authentication system

--- tests/dataTests.js
+++ tests/dataTests.js
@@ -9,3 +9,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:44.204Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:44.524Z
+// TODO: Implement functionality
