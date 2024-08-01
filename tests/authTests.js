df0420d49aa4f3c2c8c47e02639b49fb368aed44
irfanghapar <irfanghapar02@gmail.com>
2024-08-02 00:00:00 +0800

    Add unit tests for tests/authTests.js

--- tests/authTests.js
+++ tests/authTests.js
@@ -55,3 +55,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:07.447Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:09.435Z
+// TODO: Implement functionality
