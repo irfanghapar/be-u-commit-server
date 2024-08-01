3fa057aad4edd9d4946c8ec724783641e3c96b67
irfanghapar <irfanghapar02@gmail.com>
2024-08-02 00:00:00 +0800

    Refactor config/database.js for better performance

--- config/database.js
+++ config/database.js
@@ -5,3 +5,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:38.788Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:39.219Z
+// TODO: Implement functionality
