2137b80941c207fe74ecd9fec7fb5767b3d57028
irfanghapar <irfanghapar02@gmail.com>
2024-08-03 00:00:00 +0800

    Add logging system

--- config/server.js
+++ config/server.js
@@ -99,3 +99,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:09.543Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:09.766Z
+// TODO: Implement functionality
