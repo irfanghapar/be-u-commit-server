e68a879cdfe11b435ab75e3a36f674fc00a009f0
irfanghapar <irfanghapar02@gmail.com>
2024-08-04 03:38:24 +0800

    Changes for PR created on 2024-08-04

--- lib/cerberus_web/controllers/commit_controller.ex
+++ lib/cerberus_web/controllers/commit_controller.ex
@@ -4,6 +4,7 @@ defmodule CerberusWeb.CommitController do
     commits = Commit.list_commits_by_developer(id)
     json(conn, %{data: commits})
 // New line added 2024-09-22T19:32:54.518Z
+// Modified on 2024-09-22T19:38:24.348Z
 // New line added 2024-09-22T19:21:43.034Z
 
 // New line added 2024-09-22T19:21:28.784Z
