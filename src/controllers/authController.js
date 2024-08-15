081477e0303880561481a632c407e39b1a1c49ff
irfanghapar <irfanghapar02@gmail.com>
2024-08-16 00:00:00 +0800

    Update API endpoints in src/controllers/authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,3 +15,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:29.189Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:29.515Z
+// TODO: Implement functionality
