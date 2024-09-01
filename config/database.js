0881eaa1c54577dafe2f6fa8652959572c5bcae8
irfanghapar <irfanghapar02@gmail.com>
2024-09-02 00:00:00 +0800

    Fix security vulnerability in config/database.js

--- config/database.js
+++ config/database.js
@@ -127,3 +127,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:21.186Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:21.742Z
+// TODO: Implement functionality
