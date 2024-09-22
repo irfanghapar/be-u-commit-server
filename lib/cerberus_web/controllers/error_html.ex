4eff1d40db2911a25964eda9f36c6dbc98bb7bab
irfanghapar <irfanghapar02@gmail.com>
2024-09-23 03:33:13 +0800

    Changes for PR created on 2024-01-18T00:00:00+08:00

--- lib/cerberus_web/controllers/error_html.ex
+++ lib/cerberus_web/controllers/error_html.ex
@@ -7,6 +7,7 @@
   use CerberusWeb, :html
 // New line added 2024-09-22T19:21:40.890Z
 // New line added 2024-09-22T19:21:42.121Z
+// New line added 2024-09-22T19:33:13.185Z
 // New line added 2024-09-22T19:21:42.121Z
 // New line added 2024-09-22T19:21:30.563Z
   #
@@ -17,6 +18,7 @@
   #   * lib/cerberus_web/controllers/error_html/404.html.heex
 // New line added 2024-09-22T19:21:40.890Z
 // New line added 2024-09-22T19:21:39.233Z
+// New line added 2024-09-22T19:33:13.185Z
 // New line added 2024-09-22T19:30:50.996Z
 // New line added 2024-09-22T19:21:41.400Z
   # The default is to render a plain text page based on
