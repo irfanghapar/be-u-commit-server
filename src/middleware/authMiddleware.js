56f873d743c722fad06b9727ea980f65386d2c1c
irfanghapar <irfanghapar02@gmail.com>
2024-09-27 00:00:00 +0800

    Refactor src/middleware/authMiddleware.js for better performance

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,3 +3,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:02:58.497Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:02:59.471Z
+// TODO: Implement functionality
