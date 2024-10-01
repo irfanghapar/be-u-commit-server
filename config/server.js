a54cd34d4c2a20436d67ebdb14889d4fd2310579
irfanghapar <irfanghapar02@gmail.com>
2024-10-02 00:00:00 +0800

    Update API endpoints in config/server.js

--- config/server.js
+++ config/server.js
@@ -95,3 +95,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:08.333Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:08.441Z
+// TODO: Implement functionality
