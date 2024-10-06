53e0cac60040887f148c26e2edc93f6ff0655c48
irfanghapar <irfanghapar02@gmail.com>
2024-10-07 00:00:00 +0800

    Fix security vulnerability in tests/userTests.js

--- tests/userTests.js
+++ tests/userTests.js
@@ -87,3 +87,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:12.175Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:12.606Z
+// TODO: Implement functionality
