a6fa28365096b1e74bc20cabfeacaa1485b104d1
irfanghapar <irfanghapar02@gmail.com>
2024-10-12 00:00:00 +0800

    Fix security vulnerability in src/middleware/authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -15,3 +15,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:13.611Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:15.787Z
+// TODO: Implement functionality
