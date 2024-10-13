e41dc4685938ec86526642bf35082c6df5463a9e
irfanghapar <irfanghapar02@gmail.com>
2024-10-14 00:00:00 +0800

    Implement new feature: user profiles

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,3 +7,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:00.236Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:05.099Z
+// TODO: Implement functionality
