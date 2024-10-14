2afa8ac72bd126e121d9c28e5d069cabd04ab072
irfanghapar <irfanghapar02@gmail.com>
2024-10-15 00:00:00 +0800

    Update API endpoints in src/middleware/authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -33,3 +33,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:22.602Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:23.791Z
+// TODO: Implement functionality
