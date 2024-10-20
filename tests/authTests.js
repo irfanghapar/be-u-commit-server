2dce995583b80d475feb1844626db8833b6322b6
irfanghapar <irfanghapar02@gmail.com>
2024-10-21 00:00:00 +0800

    Optimize database queries in tests/authTests.js

--- tests/authTests.js
+++ tests/authTests.js
@@ -53,3 +53,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:06.140Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:07.447Z
+// TODO: Implement functionality
