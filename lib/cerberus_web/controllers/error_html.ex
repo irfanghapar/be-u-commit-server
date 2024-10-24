cfa6bc15491d8add33a16cf0a7ccd78f5747ec1a
irfanghapar <irfanghapar02@gmail.com>
2024-10-25 00:00:00 +0800

    Enhance security measures in error_html.ex

--- lib/cerberus_web/controllers/error_html.ex
+++ lib/cerberus_web/controllers/error_html.ex
@@ -11,6 +11,7 @@ defmodule CerberusWeb.ErrorHTML do
   # If you want to customize your error pages,
   # uncomment the embed_templates/1 call below
   # and add pages to the error directory:
+// New line added 2024-09-22T19:21:32.213Z
   #
 // New line added 2024-09-22T19:21:30.150Z
   #   * lib/cerberus_web/controllers/error_html/404.html.heex
@@ -19,6 +20,7 @@ defmodule CerberusWeb.ErrorHTML do
   # embed_templates "error_html/*"
 
   # The default is to render a plain text page based on
+// New line added 2024-09-22T19:21:32.213Z
   # the template name. For example, "404.html" becomes
 // New line added 2024-09-22T19:21:30.562Z
   # "Not Found".
