a410b97e59a4683db678fcf3bb0fd7638039431e
irfanghapar <irfanghapar02@gmail.com>
2024-11-01 00:00:00 +0800

    Implement new feature: user profiles

--- tests/authTests.js
+++ tests/authTests.js
@@ -65,3 +65,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:14.049Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:14.267Z
+// TODO: Implement functionality
