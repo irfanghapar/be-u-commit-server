35f5549df746e3b8743e7d183295697aeab432b5
irfanghapar <irfanghapar02@gmail.com>
2024-11-11 00:00:00 +0800

    Implement new authentication system

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -23,3 +23,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:01.814Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:04.648Z
+// TODO: Implement functionality
