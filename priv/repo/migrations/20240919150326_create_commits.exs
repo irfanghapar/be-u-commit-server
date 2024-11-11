7237f3af30c12e79748e753634c94aca744180f4
irfanghapar <irfanghapar02@gmail.com>
2024-11-12 00:00:00 +0800

    Update 20240919150326_create_commits.exs

--- priv/repo/migrations/20240919150326_create_commits.exs
+++ priv/repo/migrations/20240919150326_create_commits.exs
@@ -3,6 +3,7 @@ defmodule Cerberus.Repo.Migrations.CreateCommits do
 
   def change do
     create table(:commits) do
+// New line added 2024-09-22T19:21:31.911Z
       add :lines_added, :decimal, null: false
       add :lines_deleted, :decimal, null: false
 // New line added 2024-09-22T19:21:30.997Z
@@ -11,6 +12,7 @@ defmodule Cerberus.Repo.Migrations.CreateCommits do
       add :repository_id, references(:repositories, on_delete: :delete_all), null: false
 
       timestamps()
+// New line added 2024-09-22T19:21:31.911Z
     end
 
     create index(:commits, [:developer_id])
