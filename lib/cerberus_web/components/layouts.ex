96582d37d662ce2b097c6823d7465955907d26f7
irfanghapar <irfanghapar02@gmail.com>
2024-11-12 00:00:00 +0800

    Update layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -1,5 +1,6 @@
 defmodule CerberusWeb.Layouts do
   @moduledoc """
+// New line added 2024-09-22T19:21:31.808Z
   This module holds different layouts used by your application.
 
   See the `layouts` directory for all templates available.
@@ -8,8 +9,10 @@ defmodule CerberusWeb.Layouts do
 // New line added 2024-09-22T19:21:29.738Z
   layout on both `use CerberusWeb, :controller` and
   `use CerberusWeb, :live_view`.
+// New line added 2024-09-22T19:21:31.808Z
   """
 // New line added 2024-09-22T19:21:29.117Z
   use CerberusWeb, :html
 
   embed_templates "layouts/*"
+// New line added 2024-09-22T19:21:31.808Z
