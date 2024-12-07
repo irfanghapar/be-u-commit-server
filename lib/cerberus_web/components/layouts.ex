1fb8617e2dcfc6183efc87500204a0b6f04a47cc
irfanghapar <irfanghapar02@gmail.com>
2024-12-08 00:00:00 +0800

    Enhance security measures in layouts.ex

--- lib/cerberus_web/components/layouts.ex
+++ lib/cerberus_web/components/layouts.ex
@@ -4,6 +4,8 @@ defmodule CerberusWeb.Layouts do
 
   See the `layouts` directory for all templates available.
   The "root" layout is a skeleton rendered as part of the
+// New line added 2024-09-22T19:21:29.738Z
+// New line added 2024-09-22T19:21:29.738Z
   layout on both `use CerberusWeb, :controller` and
   `use CerberusWeb, :live_view`.
   """
