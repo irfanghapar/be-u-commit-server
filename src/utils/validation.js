5f95b10d059d59799047b357525ef82da60519c3
irfanghapar <irfanghapar02@gmail.com>
2024-12-16 00:00:00 +0800

    Improve error handling in src/utils/validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -11,3 +11,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:10.197Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:13.386Z
+// TODO: Implement functionality
