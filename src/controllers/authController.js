5a392a636cf932ea0b305639cacfc7b86c8a7e22
irfanghapar <irfanghapar02@gmail.com>
2024-12-21 00:00:00 +0800

    Improve error handling in src/controllers/authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,3 +9,5 @@
 // TODO: Implement functionality
 // Last modified: 2024-09-22T19:03:01.090Z
 // TODO: Implement functionality
+// Last modified: 2024-09-22T19:03:18.048Z
+// TODO: Implement functionality
